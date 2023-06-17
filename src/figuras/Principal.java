62c5f7fe3f7407600be2a6661d0dbcb5cbacb9c3
Sergio Romero <seromeros@unal.edu.co>
2023-06-17 11:16:34 -0500

    Ejercicio 8.3

---
+++ src/figuras/Principal.java
@@ -0,0 +1,10 @@
+package figuras;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+    }
+    
+}
